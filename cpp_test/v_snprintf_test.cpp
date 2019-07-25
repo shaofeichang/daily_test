1fb4e26e23ef171d390137387f8c46478cfc25c0
shaofeichang <chang1151230815@outlook.com>
2019-07-25 16:21:02 +0800

    添加空行

--- cpp_test/v_snprintf_test.cpp
+++ cpp_test/v_snprintf_test.cpp
@@ -2,7 +2,7 @@
 #include <stdarg.h>
 #define MAXLEN 10
 
-int mon_log(char *format, ...)
+int mon_log(const char *format, ...)
 {
     char str_tmp[MAXLEN];
     int i = 0, j = 0;
@@ -10,7 +10,7 @@ int mon_log(char *format, ...)
     va_start(vArgList, format);
     i = vsnprintf(str_tmp, MAXLEN, format, vArgList);
     va_end(vArgList);
-    printf("%s", str_tmp); //黄色高亮部分代码
+    printf("%s", str_tmp); 
     for (j = 0; j < MAXLEN; j++)
     {
         printf("%d  ", str_tmp[j]);
@@ -21,7 +21,11 @@ int mon_log(char *format, ...)
 
 int main()
 {
-    int i = mon_log("%s,%d,%d,%c", "abc", 2, 3, '\n'); //绿色高亮部分代码
+
+
+    
+    //warning: ISO C++ forbids converting a string constant to 'char*'
+    int i = mon_log("%s,%d,%d,%c", "abc", 2, 3, '\n'); 
     printf("%d\n", i);
     getchar();
     return 0;
