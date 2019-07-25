d184d0412b334f5c1600d6375d2736765f92f1f2
shaofeichang <chang1151230815@outlook.com>
2019-07-25 22:09:09 +0800

    memcpy

--- cpp_test/string.h/memcpy_example.c
+++ cpp_test/string.h/memcpy_example.c
@@ -6,28 +6,14 @@ struct
     char name[40];
     int age;
 } person, person_copy;
-struct
-{
-    char name[40];
-    double age;
-} S1;
-struct
-{
-    int a;
-    char name[40];
-    double age;
-} S2;
 
 int main()
 {
-    printf("sizeof(S1): %d\n", sizeof(S1));
-    printf("sizeof(S2): %d\n", sizeof(S2));
-
     char myname[] = "Pierre de Fermat";
     /* using memcpy to copy string: */
     memcpy(person.name, myname, strlen(myname) + 1);
     person.age = 46;
-    printf("sizeof(person): %d\n", sizeof(person));
+
     /* using memcpy to copy structure: */
     memcpy(&person_copy, &person, sizeof(person));
     printf("person_copy: %s, %d \n", person_copy.name, person_copy.age);
