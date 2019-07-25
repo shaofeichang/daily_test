39daebad52affcfba130566dcf46fb8dd5075c42
shaofeichang <chang1151230815@outlook.com>
2019-07-25 20:34:23 +0800

    stdio examples

--- cpp_test/stdio_dir/snprintf_test.cpp
+++ cpp_test/stdio_dir/snprintf_test.cpp
@@ -1,20 +1,21 @@
 #include <stdio.h>
 #include <strings.h>
 #include <string.h>
+#define NUMLEN 20
 int main(void)
 {
-    char a[10] = {'\0'};
+    char a[NUMLEN] = {'\0'};
     int i = 0;
     int ret = 0;
     memset(a, 1, sizeof(a));
-    for (i = 0; i < 10; i++)
+    for (i = 0; i < NUMLEN; i++)
     {
-        printf("a[%d] = %d  ", i, a[i]);
+        printf("a[%d] = %d ", i, a[i]);
     }
-    ret = snprintf(a, 10, "%d %s", 123, "hello");
+    ret = snprintf(a, NUMLEN, "%d %s", 123, "helloppp");
     printf("ret = %d\n", ret);
 
-    for (i = 0; i < 10; i++)
+    for (i = 0; i < NUMLEN; i++)
     {
         printf("a[%d] = %d\n", i, a[i]);
     }
--- cpp_test/stdio_dir/v_snprintf_test.cpp
+++ cpp_test/stdio_dir/v_snprintf_test.cpp
@@ -18,15 +18,30 @@ int mon_log(const char *format, ...)
     printf("\n");
     return i;
 }
+int mon_log2(char *str_tmp, const char *format, ...)
+{
+    //char str_tmp[MAXLEN];
+    int i = 0, j = 0;
+    va_list vArgList;
+    va_start(vArgList, format);
+    i = vsnprintf(str_tmp, MAXLEN, format, vArgList);
+    va_end(vArgList);
+    printf("%s", str_tmp); 
+   /*  for (j = 0; j < MAXLEN; j++)
+    {
+        printf("%d  ", str_tmp[j]);
+    } */
+    printf("\n");
+    return i;
+}
 
 int main()
 {
-
-
-    
     //warning: ISO C++ forbids converting a string constant to 'char*'
     int i = mon_log("%s,%d,%d,%c", "abc", 2, 3, '\n'); 
-    printf("%d\n", i);
+    char str[MAXLEN] = {0};
+    int j = mon_log2(str, "%s,%d,%d,%c", "abcasdfasdfasdf", 2, 3, '\n'); 
+    printf("\n %d   %d\n", i, j);
     getchar();
     return 0;
 }
