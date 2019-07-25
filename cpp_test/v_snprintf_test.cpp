bf15681ee07a02b114fac02f971df53455912eb7
shaofeichang <chang1151230815@outlook.com>
2019-07-25 08:08:01 +0800

    shift alt f format file

--- cpp_test/v_snprintf_test.cpp
+++ cpp_test/v_snprintf_test.cpp
@@ -1,18 +1,17 @@
 #include <stdio.h>
 #include <stdarg.h>
 #define MAXLEN 10
-int mon_log(char* format, ...)
+
+int mon_log(char *format, ...)
 {
     char str_tmp[MAXLEN];
-    int i=0,j=0;
-    va_list vArgList;                           
-    va_start (vArgList, format);                 
-    i=vsnprintf(str_tmp, MAXLEN, format, vArgList);  
-    va_end(vArgList);                            
-
-    printf("%s", str_tmp);  //黄色高亮部分代码
-
-    for(j=0;j<MAXLEN;j++)
+    int i = 0, j = 0;
+    va_list vArgList;
+    va_start(vArgList, format);
+    i = vsnprintf(str_tmp, MAXLEN, format, vArgList);
+    va_end(vArgList);
+    printf("%s", str_tmp); //黄色高亮部分代码
+    for (j = 0; j < MAXLEN; j++)
     {
         printf("%d  ", str_tmp[j]);
     }
@@ -21,9 +20,9 @@ int mon_log(char* format, ...)
 }
 
 int main()
-{   
-    int i=mon_log("%s,%d,%d,%c","abc",2,3,'\n');  //绿色高亮部分代码  
-    printf("%d\n",i);
+{
+    int i = mon_log("%s,%d,%d,%c", "abc", 2, 3, '\n'); //绿色高亮部分代码
+    printf("%d\n", i);
     getchar();
     return 0;
 }
