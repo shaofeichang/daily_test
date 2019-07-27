209e5525870a138862ba0a9a522e6dc9ead037b7
shaofeichang <chang1151230815@outlook.com>
2019-07-27 08:55:51 +0800

    class_dir

--- cpp_test/array_dir/array_pointer.cpp
+++ cpp_test/array_dir/array_pointer.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 using namespace std;
 float *find(float p[][4], int m);
+float *find1(float (*p)[4], int m);
 float *find2(float *p, int m);
 
 int main()
@@ -9,13 +10,12 @@ int main()
     int(*ppp)[3] = test1;
     cout << " test1: " << test1 << "\t &test1: " << &test1 << "test1[0][0]: " << test1[0][0] << endl;
     cout << *(*test1 + 1) << "|" << **(test1 + 1) << "|" << test1 + 1 << endl;
-    cout << "PPP: " << ppp <<" &ppp "<< &ppp << " "<< ppp[0][0] <<" "<< &ppp[0][0] <<" "<< *(*ppp + 1) << endl;
+    cout << "PPP: " << ppp << " &ppp " << &ppp << " " << ppp[0][0] << " " << &ppp[0][0] << " " << *(*ppp + 1) << endl;
 
     double th[3][3][3] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26};
     double(*pth)[3][3] = th;
     double ***ppth = (double ***)th;
 
-    float *find1(float(*p)[4], int m);
     float score[][4] = {{50, 51, 52, 55}, {70, 70, 40, 80}, {77, 99, 88, 67}};
     float *pf = NULL;
     cout << "sizeof(pf) = : " << sizeof(pf) << endl;
@@ -24,8 +24,8 @@ int main()
     cout << "input a number 0<num<4:";
     cin >> m;
 
-    pf = find2((float *)score, m); //返回为一维数组指针，指向一个学生成绩
-    //pf = find1(score, m);
+    // pf = find2((float *)score, m); //返回为一维数组指针，指向一个学生成绩
+    pf = find1(score, m);
     //pf = find(score, m);
     for (i = 0; i < 4; i++)
     {
