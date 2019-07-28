5cc091adcd49364bf25544e7e7cf732e6137f2a7
shaofeichang <chang1151230815@outlook.com>
2019-07-28 18:42:30 +0800

    -------

---
+++ cpp_test/other_dir/zz.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#pragma 
+union sttt
+{
+    unsigned data1[8];
+    struct datda22
+    {
+        int da1;
+        char c1;
+        short sa1;
+    }abc;
+};
+
+int main()
+{
+    sttt dasend;
+    dasend.abc.da1=
+}
