cd497714699fd1aad33508275dae122f6f6564e8
shaofeichang <chang1151230815@outlook.com>
2019-08-04 10:00:46 +0800

    call

--- lua_test/metatable_example.lua
+++ lua_test/metatable_example.lua
@@ -13,3 +13,33 @@ mytable =
 )
 
 print(mytable.key1, mytable.key2)
+
+other = {foo = 3}
+t = setmetatable({}, {__index = other})
+
+mymetatable = {}
+mytable = setmetatable({key1 = 'value1'}, {__newindex = mymetatable})
+
+print(mytable.key1)
+
+mytable.newkey = '新值2'
+print(mytable.newkey, mymetatable.newkey)
+
+mytable.key1 = '新值1'
+print(mytable.key1, mymetatable.key1)
+
+
+mytable =
+    setmetatable(
+    {key1 = 'value1'},
+    {
+        __newindex = function(mytable, key, value)
+            rawset(mytable, key, '"' .. value .. '"')
+        end
+    }
+)
+
+mytable.key1 = 'new value'
+mytable.key2 = 4
+
+print(mytable.key1, mytable.key2)
