0742d7e0f08e6be0ae18efeaec2d96a026c86fa2
shaofeichang <chang1151230815@outlook.com>
2019-09-09 21:01:20 +0800

    remove_if remove_copy_if

--- cpp_test/stl_function/lambda_example.cpp
+++ cpp_test/stl_function/lambda_example.cpp
@@ -2,26 +2,60 @@
 #include <vector>
 #include <algorithm>
 #include <functional>
+bool IsOdd(int i) { return ((i % 2) == 1); }
 int main()
 {
+    {
+
+        int myints[] = {1, 2, 3, 4, 5, 6, 7, 8, 9}; // 1 2 3 4 5 6 7 8 9
+
+        // bounds of range:
+        int *pbegin = myints;                              // ^
+        int *pend = myints + sizeof(myints) / sizeof(int); // ^                 ^
+
+        pend = std::remove_if(pbegin, pend, IsOdd); // 2 4 6 8 ? ? ? ? ?
+                                                    // ^       ^
+        std::cout << "the range contains:";
+        for (int *p = pbegin; p != pend; ++p)
+            std::cout << ' ' << *p;
+        std::cout << '\n';
+    }
+    {
+        int myints[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        std::vector<int> myvector(9);
+
+        std::remove_copy_if(myints, myints + 9, myvector.begin(), IsOdd);
+
+        std::cout << "myvector contains:";
+        for (std::vector<int>::iterator it = myvector.begin(); it != myvector.end(); ++it)
+            std::cout << ' ' << *it;
+        std::cout << '\n';
+    }
     {
         int a = 1, b = 1, c = 1;
         auto m1 = [a, &b, &c]() mutable {
             auto m2 = [a, b, &c]() mutable {
                 std::cout << a << b << c << '\n';
-                a = 4; b = 4; c = 4;
+                a = 4;
+                b = 4;
+                c = 4;
             };
-            a = 3; b = 3; c = 3;
+            a = 3;
+            b = 3;
+            c = 3;
             m2();
         };
 
-        a = 2; b = 2; c = 2;
+        a = 2;
+        b = 2;
+        c = 2;
         m1();                             // 调用 m2() 并打印 123
         std::cout << a << b << c << '\n'; // 打印 234
     }
-    std::vector<int> c = {1, 2, 3, 4, 5, 6, 7};
+    std::vector<int> c = {1, 2, 3, 4, 5, 6, 7, 2, 4};
     int x = 5;
-    c.erase(std::remove_if(c.begin(), c.end(), [x](int n) { return n < x; }), c.end());
+    std::vector<int>::iterator iter = std::remove_if(c.begin(), c.end(), [x](int n) { return n < x; });
+    c.erase(iter, c.end());
 
     std::cout << "c: ";
     std::for_each(c.begin(), c.end(), [](int i) { std::cout << i << ' '; });
