e3d3a565784a30e374b557195044afe815dcaed5
shaofeichang <chang1151230815@outlook.com>
2019-09-14 16:52:06 +0800

    quick sort

--- cpp_test/algorithm/sort_function.h
+++ cpp_test/algorithm/sort_function.h
@@ -77,8 +77,81 @@ void s_shell(vector<T> &vec)
         gap = (int)(gap / 3);
     }
 }
-template<typename T>
-void s_merge(vector<T> vec)
+template <typename T>
+vector<T> sub_merge(vector<T> vecl, vector<T> vecr)
 {
-    
+    int i = 0;
+    vector<T> result;
+    while (vecl.size() > 0 && vecr.size() > 0)
+    {
+        if (vecl[0] <= vecr[0])
+        {
+            result.push_back(vecl[0]);
+            vecl.erase(vecl.begin());
+        }
+        else
+        {
+            result.push_back(vecr[0]);
+            vecr.erase(vecr.begin());
+        }
+    }
+    while (vecl.size() > 0)
+    {
+        result.push_back(vecl[0]);
+        vecl.erase(vecl.begin());
+    }
+    while (vecr.size() > 0)
+    {
+        result.push_back(vecr[0]);
+        vecr.erase(vecr.begin());
+    }
+    return result;
+}
+template <typename T>
+vector<T> sort_merge(vector<T> vec)
+{
+    if (vec.size() < 2)
+        return vec;
+    int middle = vec.size() / 2;
+    vector<T> left(vec.begin(), vec.begin() + middle);
+    vector<T> right(vec.begin() + middle, vec.end());
+    return sub_merge(sort_merge(left), sort_merge(right));
+}
+template <typename T>
+void s_merge(vector<T> &vec)
+{
+    vec = sort_merge(vec);
+}
+template <typename T>
+int partition(vector<T> &vec, int left, int right)
+{
+    int pivot = left;
+    int index = pivot + 1;
+    for (size_t i = index; i <= right; i++)
+    {
+        if(vec[i] < vec[pivot])
+        {
+            s_swap(&vec[i], &vec[index]);
+            index ++;
+        }
+    }
+    s_swap(&vec[pivot], &vec[index - 1]);
+    return index - 1;
+}
+template <typename T>
+vector<T> sort_quick(vector<T> &vec, int left, int right)
+{
+    if(left < right)
+    {
+        int index = partition(vec, left, right);
+        sort_quick(vec, left, index - 1);
+        sort_quick(vec, index + 1, right);
+    }
+    return vec;
+}
+
+template <typename T>
+void s_quick(vector<T> &vec)
+{
+    sort_quick(vec, 0, vec.size() - 1);
 }
--- cpp_test/algorithm/sort_main.cpp
+++ cpp_test/algorithm/sort_main.cpp
@@ -3,10 +3,12 @@
 int main()
 {
     vector<int> vec = {12, 5, 61, 2, 63, 66, 22, 88};
-    //s_bubble(vec);
+    // s_bubble(vec);
     // s_selection(vec);
-    //s_insert(vec);
-    s_shell(vec);
+    // s_insert(vec);
+    // s_shell(vec);
+    // s_merge(vec);
+    s_quick(vec);
     for (auto i : vec)
         cout << i << " ";
     cout << endl;
