0e1119e13ace86394b91d35048cc098b1cc955e8
shaofeichang <chang1151230815@outlook.com>
2019-09-21 12:26:32 +0800

    snprintf添加失败案例

--- cpp_test/stdio_dir/snprintf_example.cpp
+++ cpp_test/stdio_dir/snprintf_example.cpp
@@ -9,9 +9,9 @@ int main(void)
     int ret = 0;
     memset(a, 1, sizeof(a));
     printf("\n sizeof(a): %d ,  strlen(a): %d \n ", sizeof(a), strlen(a));
-    ret = snprintf(a, NUMLEN, "%u %s", 123, "helloqwe");
+    ret = snprintf(a, NUMLEN, "%u %s", 123, "helloqweopo");//输入长度超过NUMLEN时，返回-1，传递数据成功，char数组，结尾非'\0'
     printf("ret = %d\n", ret);
-    printf("sizeof(a): %d ,  strlen(a): %d \n ", sizeof(a), strlen(a));
+    printf("sizeof(a): %d ,  strlen(a): %d \n str a : %s", sizeof(a), strlen(a), a);
     for (i = 0; i < NUMLEN; i++)
     {
         printf("a[%d] = %d  ", i, a[i]);
