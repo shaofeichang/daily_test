76a8b42aa1e5b8f2f242b5692238b1224eec638e
shaofeichang <chang1151230815@outlook.com>
2019-09-30 10:02:59 +0800

    daily

--- cpp_test/other_dir/daily_test0926.cpp
+++ cpp_test/other_dir/daily_test0926.cpp
@@ -8,9 +8,28 @@ public:
     ~TestA() {}
     int a;
 };
+void getmem(char *&p)
+{
+    p = (char*)malloc(100);
+}
+void Test(void)
+{
+    char *str = NULL;
+    getmem(str);
+    strcpy(str,"helloworld");
+    printf(str);
+    free(str);
+}
+void setarr(int *a)
+{
+    a[1] = 123;
+}
 int main()
 {
-    int num = 0x80000000;INT_MIN
+    int aaa[10] = {1,2,3,4,5};
+    setarr(aaa);
+    Test();
+    int num = 0x80000000;
     cout << sizeof(DWORD) << endl;
     TestA testa;
     int a = 123;
