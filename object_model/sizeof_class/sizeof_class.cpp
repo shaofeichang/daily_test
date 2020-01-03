a628b8df2eb10ef1ff8d830e8777a5c9572394bb
shaofeichang <chang1151230815@outlook.com>
2020-01-03 12:14:58 +0800

    --

--- object_model/sizeof_class/sizeof_class.cpp
+++ object_model/sizeof_class/sizeof_class.cpp
@@ -10,8 +10,8 @@ int main()
 	cout << "sizeof(B): " << sizeof(class_B) << endl;
 	cout << "sizeof(C): " << sizeof(class_C) << endl;
 	cout << "sizeof(D): " << sizeof(class_D) << endl;
-	cout << "sizeof(D): " << sizeof(class_E) << endl;
-	cout << "sizeof(D): " << sizeof(class_derived_E) << endl;
+	cout << "sizeof(E): " << sizeof(class_E) << endl;
+	cout << "sizeof(class_derived_E): " << sizeof(class_derived_E) << endl;
 
 	system("pause");
 	return 0;
