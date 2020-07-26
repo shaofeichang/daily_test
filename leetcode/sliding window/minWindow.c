a91dd3936d61b5cac4251da24d7d28a1cb62a522
shaofeichang <chang1151230815@outlook.com>
2020-07-26 14:52:49 +0800

    添加题目描述

--- leetcode/sliding window/minWindow.c
+++ leetcode/sliding window/minWindow.c
@@ -1,9 +1,19 @@
+/*
+   76. 最小覆盖子串
+   给你一个字符串 S、一个字符串 T，请在字符串 S 里面找出：包含 T 所有字符的最小子串。
+   示例：
+   输入: S = "ADOBECODEBANC", T = "ABC"
+   输出: "BANC"
+   说明：
+   如果 S 中不存这样的子串，则返回空字符串 ""。
+   如果 S 中存在这样的子串，我们保证它是唯一的答案。
+*/
 #define LEN  125
 char* minWindow(char* s, char* t)
 {
     int hash[LEN] = {0};
     int start=0, end=0, tLen=strlen(t), sLen=strlen(s);
-    int minStart=0, minLen=INT_MAX;
+    int minStart=0, minLen = INT_MAX;
     for(int i = 0; t[i]; i++) 
         hash[t[i]]++;	//统计 t 串字母出现的次数
     while(end < sLen) {
